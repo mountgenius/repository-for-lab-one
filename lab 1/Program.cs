6aea4504141c91fbbc9197f879aef683722dbbf7
Ryan Sasik <sasryat@dunwoody.edu>
2018-01-18 13:46:22 -0600

    change of formatting

--- lab 1/Program.cs
+++ lab 1/Program.cs
@@ -10,25 +10,30 @@ namespace lab_1
 	{
 		static void Main(string[] args)
 		{
-			while (true)
-			{
+
 
 				string[] answerKey = { "B", "D", "A", "A", "C", "A", "B", "A", "C", "D", "B", "C", "D", "A", "D", "C", "C", "B", "D", "A" };
 				string[] questions = { "question 1", "question 2", "question 3", "question 4", "question 5", "question 6", "question 7", "question 8", "question 9", "question 10", "question 11", "question 12", "question 13", "question 14", "question 15", "question 16", "question 17", "question 18", "question 19", "question 20" };
 				string options = ("A, B, C, or D?");
 				string userAnswer = ("");
-				int correctHolder = 0;
-				int incorrectHolder = 0 ;
+				int correctHolder;
+				int incorrectHolder;
 				
 				List<int> questionsMissed  = new List <int>();
 				int PASS_SCORE = 15;
-				string ans = ("");
+				int ans;
+			int EXIT = -1; //Sentinel Value
 				
 
 				Console.WriteLine("Welcome to the Minnesota DMV drivers test");
 				Console.WriteLine("The test will consist of 20 multiple questions, you must get 20 correct to pass.");
-				Console.WriteLine("Press enter to continue...");
-				Console.ReadLine();
+				Console.WriteLine("Enter 1 to enter test or -1 to exit the program");
+				ans = Convert.ToInt32(Console.ReadLine());
+				Console.Clear();
+				while (ans != EXIT)
+				{
+				correctHolder = 0;
+				incorrectHolder = 0;
 				int x = 0;
 				while (x < questions.Length)
 				{
@@ -47,7 +52,7 @@ namespace lab_1
 						questionsMissed.Add(x);
 					}
 					x++;
-
+					Console.Clear();
 				}
 				Console.WriteLine("You got " + correctHolder + " correct and " + incorrectHolder + " incorrect.");
 				if (correctHolder >= PASS_SCORE)
@@ -64,14 +69,16 @@ namespace lab_1
 
 					Console.WriteLine($" {questions[i]}");
 				}
-				Console.WriteLine("Would you like to try the test again? (Y/N)");
-				ans = Console.ReadLine().ToUpper();
-				if (ans != "Y")
+				questionsMissed.Clear();
+				Console.WriteLine("Would you like to try the test again? Press 1 to continue or -1 to exit");
+
+				ans = Convert.ToInt32(Console.ReadLine()) ;
+				/*if (ans != "Y")
 				{
 					Console.WriteLine("GoodBye");
 					break;
-				}
-			}
+				}*/
+			}//End of while loop
 		}
 	}
 }
